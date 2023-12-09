48a74f1c9deda1d671abdb51cb7e4ce3562923fe
zcemoyu <120374572+zcemoyu@users.noreply.github.com>
2023-12-09 19:28:34 +0000

    Update test_code.py

--- src/coursework2/tests/test_code.py
+++ src/coursework2/tests/test_code.py
@@ -1,1 +1,47 @@
-# Add your test specifications here
+def test_get_prepared_lad_data():
+    """
+    Test Case: Retrieve all LAD data
+
+    GIVEN the REST API design
+    WHEN a GET request is made to /api/v1/prepared_lad
+    THEN ensure that all LAD data is retrieved successfully
+    """
+
+def test_get_specific_prepared_lad_data():
+    """
+    Test Case: Retrieve specific LAD data
+
+    GIVEN the REST API design
+    WHEN a GET request is made to /api/v1/prepared_lad/{Country}/{Region}/{extract_date}
+    THEN ensure that LAD data for a specific country, region, and extract date is retrieved successfully
+    """
+
+def test_post_prepared_lad_data():
+    """
+    Test Case: Add new LAD data
+
+    GIVEN the REST API design
+    WHEN a POST request is made to /api/v1/prepared_lad
+    THEN ensure that new LAD data is added successfully
+    """
+
+def test_update_prepared_lad_data():
+    """
+    Test Case: Update existing LAD data
+
+    GIVEN the REST API design
+    WHEN a PUT request is made to /api/v1/prepared_lad/{Country}/{Region}/{extract_date}
+    THEN ensure that existing LAD data is updated successfully
+    """
+
+def test_delete_prepared_lad_data():
+    """
+    Test Case: Delete LAD data
+
+    GIVEN the REST API design
+    WHEN a DELETE request is made to /api/v1/prepared_lad/{Country}/{Region}/{extract_date}
+    THEN ensure that the specified LAD data is deleted successfully
+    """
+
+# Similar test cases can be written for other endpoints like prepared_itl, prepared_rgn, and prepared_ctry
+# These tests will validate the functionalities of retrieving, adding, updating, and deleting data for each dataset.
