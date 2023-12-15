a83be276e7b4d86bb53bf3dc96479db8ca90352e
zcemoyu <120374572+zcemoyu@users.noreply.github.com>
2023-12-15 14:48:52 +0000

    Fixed test_employee.py

--- src/coursework2/tests/test_employee.py
+++ src/coursework2/tests/test_employee.py
@@ -25,3 +25,30 @@ def testemp4():
     e = Employee(name="A N Other", title="Manager", employee_id="12345", salary=45000)
     e.salary = 19575.67
     assert e.salary == 19575.67
+    
+def test_employee_assign_department():
+    e = Employee(name="A N Other", title="Manager", employee_id="12345", salary=45000)
+    e.assign_department("IT")
+    assert e.department == "IT"
+
+def test_employee_str_representation():
+    e = Employee(name="A N Other", title="Manager", employee_id="12345", salary=45000)
+    e.assign_department("HR")
+    representation = str(e)
+    assert "A N Other" in representation
+    assert "12345" in representation
+    assert "HR" in representation
+    assert "Manager" in representation
+    assert "45000" in representation
+
+def test_employee_zero_hours_worked_monthly_salary():
+    e = Employee(name="A N Other", title="Manager", employee_id="12345", salary=45000)
+    monthly_salary = e.calculate_monthly_salary(0)  # Zero hours worked
+    assert monthly_salary == pytest.approx(e.salary / 12, rel=1e-2)
+
+def test_employee_overtime_calculation():
+    e = Employee(name="A N Other", title="Manager", employee_id="12345", salary=45000)
+    monthly_salary = e.calculate_monthly_salary(160)
+    assert monthly_salary == pytest.approx(4750, rel=1e-2)
+
+#Chatgpt was used to generate the four new tests but we chose what test to implement best for our case.
